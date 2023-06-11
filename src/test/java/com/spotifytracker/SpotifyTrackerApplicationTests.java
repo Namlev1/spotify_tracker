658df5f2b41fd52c1d280f547f4f064336255218
Mikolaj <mikipatkowski@gmail.com>
2023-06-11 21:29:36 +0200

    Init

---
+++ src/test/java/com/spotifytracker/SpotifyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotifytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
